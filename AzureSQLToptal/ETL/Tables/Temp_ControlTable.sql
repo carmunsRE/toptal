15de7277949188509b005a67e01bfeca830c0426
carmunsRE <carmunsRE@users.noreply.github.com>
2021-05-07 23:22:24 -0500

    Azure DB Project

--- AzureSQLToptal/ETL/Tables/Temp_ControlTable.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [ETL].[Temp_ControlTable] (
-    [PartitionID]     INT           NULL,
-    [SourceTableName] VARCHAR (255) NULL,
-    [FilterQuery]     VARCHAR (255) NULL,
-    [PartitionDate]   INT           NULL
-);
-
